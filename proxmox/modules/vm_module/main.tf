be233b474a4ec9a4852eada1ab70f309d1307431
Ye Lin Aung <me@yelinaung.com>
2025-02-10 09:11:35 +0800

    new Proxmox VM module wip for #4

---
+++ proxmox/modules/vm_module/main.tf
@@ -0,0 +1,92 @@
+resource "proxmox_virtual_environment_vm" "vm" {
+  acpi          = true
+  bios          = "seabios"
+  name          = var.name
+  node_name     = var.node_name
+  protection    = false
+  scsi_hardware = "virtio-scsi-single"
+  started       = true
+  tablet_device = true
+  tags          = ["linux", "ubuntu", "terraform"]
+  template      = false
+  vm_id         = var.vm_id
+
+  agent {
+    enabled = true
+    timeout = "15m"
+    trim    = false
+    type    = null
+  }
+
+  cpu {
+    cores      = var.cpu_cores
+    flags      = []
+    hotplugged = 0
+    limit      = 0
+    sockets    = 1
+    type       = "x86-64-v2-AES"
+    units      = 1024
+  }
+
+  disk {
+    aio               = "io_uring"
+    backup            = true
+    cache             = "none"
+    datastore_id      = var.disk_datastore_id
+    discard           = "ignore"
+    file_id           = null
+    interface         = "ide2"
+    iothread          = false
+    path_in_datastore = var.iso_path
+    replicate         = true
+    serial            = null
+    size              = 2
+    ssd               = false
+  }
+
+  disk {
+    aio               = "io_uring"
+    backup            = true
+    cache             = "none"
+    datastore_id      = "local-lvm"
+    discard           = "ignore"
+    file_format       = "raw"
+    file_id           = null
+    interface         = "scsi0"
+    iothread          = true
+    path_in_datastore = "vm-${var.vm_id}-disk-0"
+    replicate         = true
+    serial            = null
+    size              = var.disk_size
+    ssd               = false
+  }
+
+  memory {
+    dedicated = var.memory_dedicated
+    floating  = 0
+    shared    = 0
+  }
+
+  network_device {
+    bridge       = "vmbr0"
+    disconnected = false
+    enabled      = true
+    firewall     = true
+    mac_address  = var.mac_address
+    model        = "virtio"
+    mtu          = 0
+    queues       = 0
+    rate_limit   = 0
+    trunks       = null
+    vlan_id      = 0
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  vga {
+    clipboard = null
+    memory    = 16
+  }
+}
---
+++ proxmox/modules/vm_module/variables.tf
@@ -0,0 +1,50 @@
+variable "name" {
+  type        = string
+  description = "The name of the VM"
+}
+
+variable "node_name" {
+  type        = string
+  description = "The name of the Proxmox node"
+}
+
+variable "vm_id" {
+  type        = number
+  description = "The ID of the VM"
+}
+
+variable "memory_dedicated" {
+  type        = number
+  description = "The amount of dedicated memory for the VM (in MB)"
+}
+
+variable "disk_size" {
+  type        = number
+  description = "The size of the disk (in GB)"
+}
+
+variable "disk_datastore_id" {
+  type        = string
+  description = "The datastore ID for the disk"
+}
+
+variable "iso_path" {
+  type        = string
+  description = "The path to the ISO file"
+}
+
+variable "mac_address" {
+  default     = ""
+  type        = string
+  description = "The MAC address of the VM"
+}
+
+variable "cpu_cores" {
+  type        = number
+  description = "The number of CPU cores for the VM"
+}
+
+variable "started" {
+  type        = bool
+  description = "Whether the VM should be started"
+}
